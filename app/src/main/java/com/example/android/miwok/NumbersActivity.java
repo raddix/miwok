f23017a0b82c589eb02f9232e98c930e5208f6b0
raddix <rohitorrockstar@gmail.com>
2016-11-05 12:49:11 +0530

    Added the classes

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        System.out.print("This is fucked up");
+
+        ArrayList<String> listOfWords = new ArrayList<String>();
+        listOfWords.add("One");
+        listOfWords.add("Two");
+        listOfWords.add("Three");
+        listOfWords.add("Four");
+        listOfWords.add("Five");
+        listOfWords.add("Six");
+        listOfWords.add("Seven");
+        listOfWords.add("Eight");
+        listOfWords.add("Nine");
+        listOfWords.add("Ten");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,listOfWords);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+    }
+
+
+}
